fc8438130007887244d8e1239fc54b852dd1752f
santi12386 <santiagoozan97@gmail.com>
2015-07-14 22:25:25 -0300

    System outs

--- Tpfab/src/Luchador/Arquero.java
+++ Tpfab/src/Luchador/Arquero.java
@@ -7,13 +7,16 @@ import Armas.ArmaTipo;
 public class Arquero extends Luchador{
 	
 	public Arquero(FabricaLuchador fabrica, ArmaTipo tipo, String nombre){
+		System.out.println("Creando FabArquero");
 		this.fabrica = fabrica;
 		this.setear(tipo);
 		this.vida = 20;
 		this.nombre = nombre;
+		System.out.println("Arquero creado");
 	}
 
 	public void setear(ArmaTipo tipo) {
+		System.out.println("Seteando luchador");
 		this.arma = fabrica.PonerArma(tipo);
 		this.armadura=fabrica.PonerArmadura();
 	}
--- Tpfab/src/Luchador/Gladiador.java
+++ Tpfab/src/Luchador/Gladiador.java
@@ -6,13 +6,16 @@ import Armas.Espada;
 public class Gladiador extends Luchador{
 
 	public Gladiador(FabricaLuchador fabrica, ArmaTipo tipo, String nombre){
+		System.out.println("Creando FabGladiador");
 		this.fabrica = fabrica;
 		this.setear(tipo);
 		this.vida = 25;
 		this.nombre = nombre;
+		System.out.println("Gladiador creado");
 	}
 
 	public void setear(ArmaTipo tipo) {
+		System.out.println("Seteando luchador");
 		this.arma = fabrica.PonerArma(tipo);
 		this.armadura=fabrica.PonerArmadura();
 	}
